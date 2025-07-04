0daca90a315f4f3e6f2b2874d95db7b5e3bf0aaa
XO Aria <104579901+xo-aria@users.noreply.github.com>
2025-07-04 14:25:38 +0330

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    float num1, num2;
+
+    printf("Enter operation: ( * | / | + | - ): ");
+    scanf(" %c", &op);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+
+    switch (op)
+    {
+    case '*':
+        printf("%.2f * %.2f = %.2f", num1, num2, num1 * num2);
+        break;
+    case '/':
+        printf("%.2f / %.2f = %.2f", num1, num2, num1 / num2);
+        break;
+    case '+':
+        printf("%.2f + %.2f = %.2f", num1, num2, num1 + num2);
+        break;
+    case '-':
+        printf("%.2f - %.2f = %.2f", num1, num2, num1 - num2);
+        break;
+    default:
+        printf("Unknown Operation.");
+        break;
+    }
+
+    return 0;
+}
